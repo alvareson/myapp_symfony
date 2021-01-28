93fc2ddc0994e02207e0980152fa4ed396125153
Alister <urta.alister@gmail.com>
2021-01-28 13:19:26 +0500

    added CommentController

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,7 +4,23 @@
 namespace App\Controller;
 
 
-class CommentController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/comments/{id}/vote/{direction}")
+     */
+    public function commentVote($id, $direction) {
+        // todo use id to query DataBase
+        if ($direction == 'up') {
+            $currentVoteCount = rand(7, 100);
+        } else {
+            $currentVoteCount = rand(0, 5);
+        }
+
+        return new JsonResponse(['votes' => $currentVoteCount]);
+    }
 }
