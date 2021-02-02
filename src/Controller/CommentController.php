31410b694887bfb1aa26097967cfa5a52d11fdeb
Alister <alvarohvze@gmail.com>
2021-02-02 10:22:06 +0500

    Checking autowiring

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction) {
+    public function commentVote($id, $direction, LoggerInterface $logger) {
         // todo use id to query DataBase
+
         if ($direction == 'up') {
+            $logger->info('Voting Up!');
             $currentVoteCount = rand(7, 99);
         } else {
+            $logger->info('Voting Down!');
             $currentVoteCount = rand(0, 5);
         }
 
